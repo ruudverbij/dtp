2316fc1910858b39f29ee7eac29103ab39b371f2
Niek Tax <niek.tax@avanade.com>
2014-07-31 14:18:45 +0200

    python scriptje

---
+++ code/LockingVergelijker/src/testRuns/calculate_process_time.py
@@ -0,0 +1,33 @@
+import numpy as np
+import inspect, os
+import sys
+import csv
+
+f = open('data.txt')
+reader = csv.reader(f)
+writer = csv.writer(open('newfile.txt', 'w+'))
+
+bankIter = 0
+banks    = 0
+trans    = 0
+locking  = ''
+maxTime  = 0
+avgExc   = 0
+
+rowzero  = True
+for row in reader:
+	if bankIter == 0:
+		if not rowzero:
+			writer.writerow([banks, trans, locking, avgExc/banks, maxTime])
+		banks    = int(row[0])
+		bankIter = int(row[0])
+		trans    = int(row[1])
+		locking  = row[2]
+		maxTime  = 0
+		avgExc   = 0
+	else:
+		bankIter = bankIter - 1
+		avgExc   = avgExc + int(row[3])
+		maxTime  = max(maxTime, int(row[4]))
+	rowzero = False
+f.close()
